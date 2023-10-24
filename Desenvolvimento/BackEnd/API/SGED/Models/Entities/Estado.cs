42a8da14fc4f6abbd189038e4dd1b62c096d6825
Guilherme Angelo <100084412+Gui-Angelo-Silva@users.noreply.github.com>
2023-10-24 10:29:24 -0300

    Update Estado.cs

--- Desenvolvimento/BackEnd/API/SGED/Models/Entities/Estado.cs
+++ Desenvolvimento/BackEnd/API/SGED/Models/Entities/Estado.cs
@@ -2,13 +2,7 @@
 public class Estado
 {
     public int Id { get; set; }
-<<<<<<< HEAD
     public string NomeEstado { get; set; }
     public string UfEstado { get; set; }
-
-=======
-    public string? NomeEstado { get; set; }
-    public string? UfEstado { get; set; }
->>>>>>> 0f6583d1afdf60c67b850bf4e5a7199aa0aff478
     public ICollection<Cidade>? Cidades { get; set; }
 }

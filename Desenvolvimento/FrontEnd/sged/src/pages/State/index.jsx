d5f414cf72a013b6b39b9efa75c880a6d55557af
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-11-13 11:56:57 -0300

    :lipstick: feat: Estilizando a tela Cadastro e Estado

--- Desenvolvimento/FrontEnd/sged/src/pages/State/index.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/State/index.jsx
@@ -4,6 +4,7 @@ import axios from "axios"
 import SideBar from "../../components/SideBar"
 import NavBar from "../../components/NavBar"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Link } from "react-router-dom"
 
 export default function State() {
 
@@ -118,12 +119,37 @@ export default function State() {
                 <div style={{ flex: 0, width: '200px' }}>
                     <SideBar /> {/* Sidebar à esquerda */}
                 </div>
-                <div style={{ flex: 2, marginLeft: '80px', marginRight: '40px' }}>
+                <div style={{ flex: 2, marginLeft: '80px', marginRight: '40px', marginTop: -5 }}>
                     <br />
-                    <h3>Lista de Estado</h3>
-                    <header>
+                    <div className="flex flex-row">
+                        <Link to="/registration">
+                            <h3 className="text-2xl font-semibold text-gray-600 pr-2">Cadastro</h3>
+                        </Link>
+                        <h3 className="text-2xl font-semibold text-gray-600 pr-2">/</h3>
+                        <h3 className="text-2xl font-semibold text-gray-600">Estado</h3>
+                        
+                    </div>
+                    {/* <div className="bg-slate-200 rounded-md mb-10" style={{ marginTop: 15 }}>
+                        <h4 className="pl-4 pt-2 pb-2 text-gray-500">Funções</h4>
+                    </div> */}
+                    <div className="flex justify-center">
+                        <div className="w-50 h-20">
+                            <label for="default-search" className="mb-5 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                            <div className="relative">
+                                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    </svg>
+                                </div>
+                                <input type="search" id="default-search" className="block w-full pt-3 pb-3 pl-10 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-600 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquisar estado" required />
+                                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-emerald-600 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pesquisar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{}}>
                         <button className="btn btn-success" onClick={() => openCloseModalInsert()}>Adicionar</button>
-                    </header>
+                    </div>
+
                     <table className="table table-bordered">
                         <thead>
                             <tr>

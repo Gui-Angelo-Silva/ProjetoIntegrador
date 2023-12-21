1a47bfceb0f05d1e84d35858da9fdb827267ef20
JoaoNeto132 <joaomasp12@gmail.com>
2023-12-21 19:58:18 -0300

    dev: :sparkles: Criando Service Interface de TipoProcesso

---
+++ Desenvolvimento/BackEnd/API/SGED/Services/Interfaces/ITipoProcesso.cs
@@ -0,0 +1,13 @@
+﻿using SGED.DTO.Entities;
+
+namespace SGED.Services.Interfaces
+{
+    public interface ITipoBairroService
+    {
+        Task<IEnumerable<TipoBairroDTO>> GetAll();
+        Task<TipoBairroDTO> GetById(int id);
+        Task Create(TipoBairroDTO TipoBairroDTO);
+        Task Update(TipoBairroDTO TipoBairroDTO);
+        Task Remove(int id);
+    }
+}

9d78e51c8e2d681d24b20e5b6fc8fbd5939736e4
JoaoNeto132 <joaomasp12@gmail.com>
2023-12-21 19:38:25 -0300

    dev: :sparkles: Criando Model de Bairro

---
+++ Desenvolvimento/BackEnd/API/SGED/Models/Entities/Bairro.cs
@@ -0,0 +1,19 @@
+﻿using SGED.DTO.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.Runtime.CompilerServices;
+
+namespace SGED.Models.Entities
+{
+    [Table("Bairro")]
+    public class Bairro
+    {
+        [Column("idBairro")]
+        public int Id { get; set; }
+
+        [Column("nomeBairro")]
+        public String NomeBairro { get; set; }
+
+    }
+}

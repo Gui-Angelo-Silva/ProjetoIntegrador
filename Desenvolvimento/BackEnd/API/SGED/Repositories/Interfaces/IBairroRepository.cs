8f15f7e8a6ed03461437a3901a2928afc10cc2a1
JoaoNeto132 <joaomasp12@gmail.com>
2023-12-21 19:52:36 -0300

    dev: :sparkles: Atualizando Startup

---
+++ Desenvolvimento/BackEnd/API/SGED/Repositories/Interfaces/IBairroRepository.cs
@@ -0,0 +1,12 @@
+﻿using SGED.Models.Entities;
+using SGED.DTO.Entities;
+
+namespace SGED.Repositories.Interfaces;
+public interface IBairroRepository
+{
+    Task<IEnumerable<Bairro>> GetAll();
+    Task<Bairro> GetById(int id);
+    Task<Bairro> Create(Bairro Bairro);
+    Task<Bairro> Update(Bairro Bairro);
+    Task<Bairro> Delete(int id);
+}

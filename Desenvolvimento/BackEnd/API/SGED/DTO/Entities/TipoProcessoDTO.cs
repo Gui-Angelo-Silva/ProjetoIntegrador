b5fe06e9499f143830d0afc004f59f4c10bc01a2
JoaoNeto132 <joaomasp12@gmail.com>
2023-12-21 20:04:44 -0300

    dev: :sparkles: Criando DTO de TipoProcesso

---
+++ Desenvolvimento/BackEnd/API/SGED/DTO/Entities/TipoProcessoDTO.cs
@@ -0,0 +1,25 @@
+﻿using SGED.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.Xml;
+using System.Text.Json.Serialization;
+
+namespace SGED.DTO.Entities
+{
+    public class TipoProcessoDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome da TipoProcesso é requerida!")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string NomeTipoProcesso { get; set; }
+
+        [JsonIgnore]
+        public CidadeDTO? CidadeDTO { get; set; }
+
+        [Required(ErrorMessage = "A Cidade é requerida!")]
+        public int IdCidade { get; set; }
+
+    }
+}

e1360be542174121fd1daee683314d4376cf7904
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-12 11:27:46 -0300

    :lipstick: feat: Estilizando a tela inicial de Cadastro

--- Desenvolvimento/FrontEnd/sged/src/pages/routeA/dashboard/registration/index.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/routeA/dashboard/registration/index.jsx
@@ -1,8 +1,12 @@
 import NavBar from "../../components/NavBar";
 import SideBar from "../../components/SideBar";
+import ImgImovel from "../../../../assets/imgImovel.png"
 import ImgEstado from "../../../../assets/imgEstado.png";
-import ImgCidade from "../../../../assets/ImgCidade2.png";
+import ImgCidade from "../../../../assets/ImgCidade.png";
+import ImgLogradouro from "../../../../assets/ImgLogradouro.png";
+import ImgTipoLogradouro from "../../../../assets/ImgTipoLogradouro.png";
 import ImgUsuario from "../../../../assets/ImgUsuario.png";
+import ImgBairro from "../../../../assets/ImgBairro.png";
 import ImgTipoUsuario from "../../../../assets/ImgTipoUsuario.png";
 import { Link } from "react-router-dom";
 
@@ -20,48 +24,57 @@ export default function Registrations() {
                         <div className="bg-slate-200 rounded-md" style={{ marginTop: 15 }}>
                             <h4 className="pl-4 pt-2 pb-2 text-gray-500">Funções</h4>
                         </div>
-                        <div style={{ paddingTop: 40 }}>
-                            <div className="flex flex-row">
-                                <Link to="/a/state">
-                                <div className="flex flex-col ml-12 bg-[#DAEEEE] hover:bg-[#58AFAE] text-green-800 hover:scale-105" style={{ width: 229, border: '2px solid #58AFAE', height: 229, justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
-                                        <p className="text-2xl pb-2 font-medium" style={{ textAlign: 'center' }}>
-                                            Estado
-                                        </p>
-                                        <div style={{ width: 165, height: 104, alignItems: 'center' }}>
-                                            <img src={ImgEstado} alt="Abrir Cadastro de Estado" />
-                                        </div>
+                        <div className="flex">
+                            <div className="pr-[125px]">
+                                <div className="pt-4 text-xl font-semibold text-gray-600 pb-2">Imóvel</div>
+                                <div className="grid grid-cols-2">
+                                    <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#DAEEEE] mb-2 rounded-xl border-2 border-[#58AFAE] mr-4">
+                                        <div className="text-lg font-semibold text-[#005A66]">Imóvel</div>
+                                        <img src={ImgImovel} alt="Abrir tela de cadastro de imóvel" />
                                     </div>
-                                </Link>
-                                <Link to="/a/city">
-                                    <div className="flex flex-col ml-12 bg-[#DAEEEE] hover:bg-[#58AFAE] text-green-800 hover:scale-105" style={{ width: 229, border: '2px solid #58AFAE', height: 229, justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
-                                        <p className="text-2xl pb-2 font-medium" style={{ textAlign: 'center' }}>
-                                            Cidade
-                                        </p>
-                                        <div style={{ width: 165, height: 104, alignItems: 'center' }}>
-                                            <img src={ImgCidade} alt="Abrir Cadastro de Estado" />
-                                        </div>
+                                    <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#DAEEEE] mb-2 rounded-xl border-2 border-[#58AFAE]">
+                                        <div className="text-lg font-semibold text-[#005A66]">Estado</div>
+                                        <img src={ImgEstado} alt="Abrir tela de cadastro de estado" />
                                     </div>
-                                </Link>
-                                <Link to="/a/typeuser">
-                                    <div className="flex flex-col ml-12 bg-[#DAEEEE] hover:bg-[#58AFAE] text-green-800 hover:scale-105" style={{ width: 229, border: '2px solid #58AFAE', height: 229, justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
-                                        <p className="text-2xl pb-2 font-medium" style={{ textAlign: 'center' }}>
-                                            Tipo Usuário
-                                        </p>
-                                        <div style={{ width: 95, height: 104, alignItems: 'center' }}>
-                                            <img src={ImgTipoUsuario} alt="Abrir Cadastro de Estado" />
-                                        </div>
+                                    <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#DAEEEE] mb-2 rounded-xl border-2 border-[#58AFAE]">
+                                        <div className="text-lg font-semibold text-[#005A66]">Cidade</div>
+                                        <img src={ImgCidade} alt="Abrir tela de cadastro de estado" />
                                     </div>
-                                </Link>
-                                <Link to="/a/user">
-                                    <div className="flex flex-col ml-12 bg-[#DAEEEE] hover:bg-[#58AFAE] text-green-800 hover:scale-105" style={{ width: 229, border: '2px solid #58AFAE', height: 229, justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
-                                        <p className="text-2xl pb-2 font-medium" style={{ textAlign: 'center' }}>
-                                            Usuário
-                                        </p>
-                                        <div style={{ width: 104, height: 104, alignItems: 'center' }}>
-                                            <img src={ImgUsuario} alt="Abrir Cadastro de Estado" />
-                                        </div>
+                                    <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#DAEEEE] mb-2 rounded-xl border-2 border-[#58AFAE]">
+                                        <div className="text-lg font-semibold text-[#005A66]">Bairro</div>
+                                        <img src={ImgBairro} alt="Abrir tela de cadastro de estado" />
                                     </div>
-                                </Link>
+                                    <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#DAEEEE] mb-2 rounded-xl border-2 border-[#58AFAE]">
+                                        <div className="text-lg font-semibold text-[#005A66]">Logradouro</div>
+                                        <img src={ImgLogradouro} alt="Abrir tela de cadastro de estado" />
+                                    </div>
+                                    <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#DAEEEE] mb-2 rounded-xl border-2 border-[#58AFAE]">
+                                        <div className="text-lg font-semibold text-[#005A66]">Tipo Logradouro</div>
+                                        <img src={ImgTipoLogradouro} alt="Abrir tela de cadastro de estado" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="pr-[150px]">
+                                <div className="pt-4 text-xl font-semibold text-gray-600 pb-2">Usuário</div>
+                                <div className="grid grid-cols-2">
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D1EBFE] mb-2 rounded-xl border-2 border-[#42A3ED] mr-4">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D1EBFE] mb-2 rounded-xl border-2 border-[#42A3ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D1EBFE] mb-2 rounded-xl border-2 border-[#42A3ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D1EBFE] mb-2 rounded-xl border-2 border-[#42A3ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D1EBFE] mb-2 rounded-xl border-2 border-[#42A3ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D1EBFE] mb-2 rounded-xl border-2 border-[#42A3ED]">Gui</div>
+                                </div>
+                            </div>
+                            <div className="">
+                                <div className="pt-4 text-xl font-semibold text-gray-600 pb-2">Processo</div>
+                                <div className="grid grid-cols-2">
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D6D3FA] mb-2 rounded-xl border-2 border-[#4E42ED] mr-4">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D6D3FA] mb-2 rounded-xl border-2 border-[#4E42ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D6D3FA] mb-2 rounded-xl border-2 border-[#4E42ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D6D3FA] mb-2 rounded-xl border-2 border-[#4E42ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D6D3FA] mb-2 rounded-xl border-2 border-[#4E42ED]">Gui</div>
+                                    <div className="flex items-center justify-center w-[148px] h-[148px] bg-[#D6D3FA] mb-2 rounded-xl border-2 border-[#4E42ED]">Gui</div>
+                                </div>
                             </div>
                         </div>
                     </div>

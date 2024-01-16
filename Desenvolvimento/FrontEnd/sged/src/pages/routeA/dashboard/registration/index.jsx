d15cb56d13d2b4289a419747018fa7ecbc89f711
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-16 11:28:22 -0300

    :lipstick: feat: Tela de cadastro estilizado

--- Desenvolvimento/FrontEnd/sged/src/pages/routeA/dashboard/registration/index.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/routeA/dashboard/registration/index.jsx
@@ -18,8 +18,22 @@ import ImgEtapa from "../../../../assets/ImgEtapaAtualizada.png";
 import ImgTipoDocumento from "../../../../assets/ImgTipoDocumentoAtualizada.png";
 import ImgDocumentoProcesso from "../../../../assets/ImgDocumentoProcessoAtualizada.png";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 export default function Registrations() {
+    const [isHoveredImovel, setIsHoveredImovel] = useState(false);
+    const [isHoveredEstado, setIsHoveredEstado] = useState(false);
+    const [isHoveredCidade, setIsHoveredCidade] = useState(false);
+    const [isHoveredBairro, setIsHoveredBairro] = useState(false);
+    const [isHoveredLogradouro, setIsHoveredLogradouro] = useState(false);
+    const [isHoveredTipoLogradouro, setIsHoveredTipoLogradouro] = useState(false);
+
+    const [isHoveredUsuário, setIsHoveredUsuário] = useState(false);
+    const [isHoveredTipoUsuário, setIsHoveredTipoUsuário] = useState(false);
+    const [isHoveredMunicipe, setIsHoveredMunicipe] = useState(false);    
+    const [isHoveredEngenheiro, setIsHoveredEngenheiro] = useState(false);    
+    const [isHoveredFiscal, setIsHoveredFiscal] = useState(false); 
+    const [isHoveredAuditoria, setIsHoveredAuditoria] = useState(false); 
 
     return (
         <div className="flex flex-1 min-h-screen">
@@ -38,39 +52,57 @@ export default function Registrations() {
                                 <div className="pt-4 text-xl font-semibold text-gray-600 pb-2">Imóvel</div>
                                 <div className="grid grid-cols-2">
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#58AFAE] hover:scale-105 shadow-xl mb-3 rounded-xl mr-4">
-                                            <div className="text-lg font-semibold text-[#005A66]">Imóvel</div>
-                                            <img src={ImgImovel} alt="Abrir tela de cadastro de imóvel" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#005A66] hover:scale-105 shadow-xl mb-3 rounded-xl mr-4 text-lg font-semibold text-[#005A66] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredImovel(true)}
+                                            onMouseLeave={() => setIsHoveredImovel(false)}
+                                        >
+                                            Imóvel
+                                            <img src={ImgImovel} alt="Abrir tela de cadastro de imóvel" style={{ filter: isHoveredImovel ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/state">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#58AFAE] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#005A66]">Estado</div>
-                                            <img src={ImgEstado} alt="Abrir tela de cadastro de estado" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#005A66] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#005A66] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredEstado(true)}
+                                            onMouseLeave={() => setIsHoveredEstado(false)}
+                                        >
+                                            Estado
+                                            <img src={ImgEstado} alt="Abrir tela de cadastro de estado" style={{ filter: isHoveredEstado ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/city">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#58AFAE] hover:scale-105 shadow-xl mb-3 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#005A66]">Cidade</div>
-                                            <img src={ImgCidade} alt="Abrir tela de cadastro de estado" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#005A66] hover:scale-105 shadow-xl mb-3 rounded-xl text-lg font-semibold text-[#005A66] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredCidade(true)}
+                                            onMouseLeave={() => setIsHoveredCidade(false)}
+                                        >
+                                            Cidade
+                                            <img src={ImgCidade} alt="Abrir tela de cadastro de cidade" style={{ filter: isHoveredCidade ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#58AFAE] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#005A66]">Bairro</div>
-                                            <img src={ImgBairro} alt="Abrir tela de cadastro de estado" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#005A66] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#005A66] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredBairro(true)}
+                                            onMouseLeave={() => setIsHoveredBairro(false)}
+                                        >
+                                            Bairro
+                                            <img src={ImgBairro} alt="Abrir tela de cadastro de bairro" style={{ filter: isHoveredBairro ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#58AFAE] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#005A66]">Logradouro</div>
-                                            <img src={ImgLogradouro} alt="Abrir tela de cadastro de estado" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#005A66] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#005A66] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredLogradouro(true)}
+                                            onMouseLeave={() => setIsHoveredLogradouro(false)}
+                                        >
+                                            Logradouro
+                                            <img src={ImgLogradouro} alt="Abrir tela de cadastro de logradouro" style={{ filter: isHoveredLogradouro ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/typepublicplace">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#58AFAE] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#005A66]">Tipo Logradouro</div>
-                                            <img src={ImgTipoLogradouro} alt="Abrir tela de cadastro de estado" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#c8d9db] hover:bg-[#005A66] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#005A66] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredTipoLogradouro(true)}
+                                            onMouseLeave={() => setIsHoveredTipoLogradouro(false)}
+                                        >
+                                            Tipo Logradouro
+                                            <img src={ImgTipoLogradouro} alt="Abrir tela de cadastro de tipo logradouro" style={{ filter: isHoveredTipoLogradouro ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                 </div>
@@ -79,39 +111,57 @@ export default function Registrations() {
                                 <div className="pt-4 text-xl font-semibold text-gray-600 pb-2">Usuário</div>
                                 <div className="grid grid-cols-2">
                                     <Link to="/a/user">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#c0d5d9] hover:scale-105 shadow-xl mb-3 rounded-xl mr-4">
-                                            <div className="text-lg font-semibold text-[#4DA8B6]">Usuário</div>
-                                            <img src={ImgUsuario} alt="" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#4DA8B6] hover:scale-105 shadow-xl mb-3 rounded-xl mr-4 text-lg font-semibold text-[#4DA8B6] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredUsuário(true)}
+                                            onMouseLeave={() => setIsHoveredUsuário(false)}
+                                        >
+                                            Usuário
+                                            <img src={ImgUsuario} alt="Abrir tela de cadastro de usuário" style={{ filter: isHoveredUsuário ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/typeuser">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#c0d5d9] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#4DA8B6]">Tipo Usuário</div>
-                                            <img src={ImgTipoUsuario} alt="" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#4DA8B6] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#4DA8B6] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredTipoUsuário(true)}
+                                            onMouseLeave={() => setIsHoveredTipoUsuário(false)}
+                                        >
+                                            Tipo Usuário
+                                            <img src={ImgTipoUsuario} alt="Abrir tela de cadastro de tipo usuário" style={{ filter: isHoveredTipoUsuário ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#c0d5d9] hover:scale-105 shadow-xl mb-3 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#4DA8B6]">Munícipe</div>
-                                            <img src={ImgMunicipe} alt="" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#4DA8B6] hover:scale-105 shadow-xl mb-3 rounded-xl text-lg font-semibold text-[#4DA8B6] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredMunicipe(true)}
+                                            onMouseLeave={() => setIsHoveredMunicipe(false)}
+                                        >
+                                            Munícipe
+                                            <img src={ImgMunicipe} alt="Abrir tela de cadastro de municipe" style={{ filter: isHoveredMunicipe ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#c0d5d9] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#4DA8B6]">Engenheiro</div>
-                                            <img src={ImgEngenheiro} alt="" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#4DA8B6] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#4DA8B6] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredEngenheiro(true)}
+                                            onMouseLeave={() => setIsHoveredEngenheiro(false)}
+                                        >
+                                            Engenheiro
+                                            <img src={ImgEngenheiro} alt="Abrir tela de cadastro de engenheiro" style={{ filter: isHoveredEngenheiro ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#c0d5d9] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#4DA8B6]">Fiscal</div>
-                                            <img src={ImgFiscal} alt="" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#4DA8B6] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#4DA8B6] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredFiscal(true)}
+                                            onMouseLeave={() => setIsHoveredFiscal(false)}
+                                        >
+                                            Fiscal
+                                            <img src={ImgFiscal} alt="Abrir tela de cadastro de fiscal" style={{ filter: isHoveredFiscal ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                     <Link to="/a/development">
-                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#c0d5d9] hover:scale-105 shadow-xl mb-2 rounded-xl">
-                                            <div className="text-lg font-semibold text-[#4DA8B6]">Auditoria</div>
-                                            <img src={ImgAuditoria} alt="" />
+                                        <div className="flex flex-col items-center justify-center w-[148px] h-[148px] bg-[#cde3e7] hover:bg-[#4DA8B6] hover:scale-105 shadow-xl mb-2 rounded-xl text-lg font-semibold text-[#4DA8B6] hover:text-white"
+                                            onMouseEnter={() => setIsHoveredAuditoria(true)}
+                                            onMouseLeave={() => setIsHoveredAuditoria(false)}
+                                        >
+                                            <div className="">Auditoria</div>
+                                            <img src={ImgAuditoria} alt="abrir tela de cadastro de auditoria" style={{ filter: isHoveredAuditoria ? 'brightness(0) invert(1)' : 'none' }}/>
                                         </div>
                                     </Link>
                                 </div>

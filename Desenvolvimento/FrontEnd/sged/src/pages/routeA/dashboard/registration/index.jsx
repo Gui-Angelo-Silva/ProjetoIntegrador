50783f1a0681a041e7ce0a275a25f9c7e7cabc68
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-23 10:17:59 -0300

    :lipstick: feet: Adicionando transparência na label de pesquisa tela registration

--- Desenvolvimento/FrontEnd/sged/src/pages/routeA/dashboard/registration/index.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/routeA/dashboard/registration/index.jsx
@@ -83,7 +83,7 @@ export default function Registrations() {
                         <br />
                         <h3 className="text-2xl font-semibold text-gray-600">Cadastros</h3>
                         <div className="bg-slate-200 rounded-md" style={{ marginTop: 15 }}>
-                            <div className="flex relative border rounded-lg border-[#BCBCBC]">
+                            <div className="flex relative border rounded-lg border-[#BCBCBC] bg-white">
                                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                     <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />

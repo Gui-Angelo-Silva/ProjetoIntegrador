eb55f385242b1517a8eae94980be1e8148b79909
JoaoNeto132 <joaomasp12@gmail.com>
2024-02-23 14:58:12 -0300

    :sparkless: BackEnd: Criado DTO de Engenheiro!

---
+++ Desenvolvimento/BackEnd/API/SGED/DTO/Entities/EngenheiroDTO.cs
@@ -0,0 +1,14 @@
+﻿using SGED.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.Xml;
+using System.Text.Json.Serialization;
+
+namespace SGED.DTO.Entities
+{
+    public class EngenheiroDTO : PessoaDTO
+    {
+        public int Id { get; set; }
+        public string crea { get; set; }
+    }
+}

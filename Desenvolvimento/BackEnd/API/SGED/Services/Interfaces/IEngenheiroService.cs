fd0126f927b52cb5d765a33e1285b10682a237ba
JoaoNeto132 <joaomasp12@gmail.com>
2024-02-23 15:05:42 -0300

    :sparkless: BackEnd: Criando Interface Service de Engenheiro!

---
+++ Desenvolvimento/BackEnd/API/SGED/Services/Interfaces/IEngenheiroService.cs
@@ -0,0 +1,13 @@
+﻿using SGED.DTO.Entities;
+
+namespace SGED.Services.Interfaces
+{
+    public interface IEngenheiroService
+    {
+        Task<IEnumerable<EngenheiroDTO>> GetAll();
+        Task<EngenheiroDTO> GetById(int id);
+        Task Create(EngenheiroDTO engenheiroDTO);
+        Task Update(EngenheiroDTO engenheiroDTO);
+        Task Remove(int id);
+    }
+}

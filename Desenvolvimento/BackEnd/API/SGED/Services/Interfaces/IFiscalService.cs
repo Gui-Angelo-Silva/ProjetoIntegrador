d42efd4a7ec2e4035368939004fe621c9f59d77d
EduardoLoppes <mcfgonzalles@gmail.com>
2024-02-27 11:11:10 -0300

    :sparkles: Services de fiscal

---
+++ Desenvolvimento/BackEnd/API/SGED/Services/Interfaces/IFiscalService.cs
@@ -0,0 +1,13 @@
+﻿using SGED.DTO.Entities;
+
+namespace SGED.Services.Interfaces
+{
+    public interface IFiscalService
+    {
+        Task<IEnumerable<FiscalDTO>> GetAll();
+        Task<FiscalDTO> GetById(int id);
+        Task Create(FiscalDTO fiscalDTO);
+        Task Update(FiscalDTO fiscalDTO);
+        Task Remove(int id);
+    }
+}

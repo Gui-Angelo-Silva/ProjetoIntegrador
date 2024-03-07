9f30b0d3bc9baa0a0c5ce2f49beb9a2be324b162
JoaoNeto132 <joaomasp12@gmail.com>
2024-03-07 12:57:20 -0300

    :sparkless: Backend: Service Entities de tipoprocessoetapa desenvolvida!

--- Desenvolvimento/BackEnd/API/SGED/Controllers/TipoprocessoEtapa.cs
+++ Desenvolvimento/BackEnd/API/SGED/Controllers/TipoprocessoEtapa.cs
@@ -11,41 +11,41 @@ namespace SGED.Controllers
     [Route("api/[controller]")]
     [ApiController]
     //[Authorize("ApiScope")]
-    public class BairroController : Controller
+    public class TipoProcessoEtapa : Controller
     {
 
-        private readonly IBairroService _BairroService;
+        private readonly ITipoProcessoEtapaService _TipoProcessoEtapaService;
 
-        public BairroController(IBairroService BairroService)
+        public TipoProcessoEtapa(ITipoProcessoEtapaService TipoProcessoEtapaService)
         {
-            _BairroService = BairroService;
+			_TipoProcessoEtapaService = TipoProcessoEtapaService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<BairroDTO>>> Get()
+        public async Task<ActionResult<IEnumerable<TipoProcessoEtapaDTO>>> Get()
         {
-            var bairrosDTO = await _BairroService.GetAll();
-            return Ok(bairrosDTO);
+            var tipoProcessoEtapaDTO = await _TipoProcessoEtapaService.GetAll();
+            return Ok(tipoProcessoEtapasDTO);
         }
 
         [HttpGet("{id}", Name = "GetBairro")]
         public async Task<ActionResult<BairroDTO>> Get(int id)
         {
-            var BairroDTO = await _BairroService.GetById(id);
-            if (BairroDTO == null) return NotFound("Bairro não encontrada");
-            return Ok(BairroDTO);
+            var TipoProcessoEtapaDTO = await _TipoProcessoEtapaService.GetById(id);
+            if (TipoProcessoEtapaDTO == null) return NotFound("TipoProcessoEtapa não encontrado");
+            return Ok(TipoProcessoEtapaDTO);
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post([FromBody] BairroDTO BairroDTO)
+        public async Task<ActionResult> Post([FromBody] TipoProcessoEtapaDTO TipoProcessoEtapaDTO)
         {
-            if (BairroDTO is null) return BadRequest("Dado inválido!");
-            await _BairroService.Create(BairroDTO);
-            return new CreatedAtRouteResult("GetBairro", new { id = BairroDTO.Id }, BairroDTO);
+            if (TipoProcessoEtapaDTO is null) return BadRequest("Dado inválido!");
+            await _TipoProcessoEtapaService.Create(TipoProcessoEtapaDTO);
+            return new CreatedAtRouteResult("GetTipoProcessoEtapa", new { id = TipoProcessoEtapaDTO.Id }, TipoProcessoEtapaDTO);
         }
 
         [HttpPut()]
-        public async Task<ActionResult> Put([FromBody] BairroDTO BairroDTO)
+        public async Task<ActionResult> Put([FromBody] TipoProcessoEtapaDTO TipoProcessoEtapaDTO)
         {
             if (BairroDTO is null) return BadRequest("Dado invalido!");
             await _BairroService.Update(BairroDTO);
@@ -53,12 +53,12 @@ namespace SGED.Controllers
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<BairroDTO>> Delete(int id)
+        public async Task<ActionResult<TipoProcessoEtapaDTO>> Delete(int id)
         {
-            var BairroDTO = await _BairroService.GetById(id);
-            if (BairroDTO == null) return NotFound("Bairro não econtrado!");
-            await _BairroService.Remove(id);
-            return Ok(BairroDTO);
+            var TipoProcessoEtapaDTO = await _TipoProcessoEtapaService.GetById(id);
+            if (TipoProcessoEtapaDTO == null) return NotFound("TipoProcessoEtapa não econtrado!");
+            await _TipoProcessoEtapaService.Remove(id);
+            return Ok(TipoProcessoEtapaDTO);
         }
     }
 }

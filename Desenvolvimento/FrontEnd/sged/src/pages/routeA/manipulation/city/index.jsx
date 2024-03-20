9aa672b97279dd04c81eaa647561e9f298f25d9d
EduardoLoppes <mcfgonzalles@gmail.com>
2024-03-20 09:01:46 -0300

    :recycle: mudando span para div da classe city

--- Desenvolvimento/FrontEnd/sged/src/pages/routeA/manipulation/city/index.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/routeA/manipulation/city/index.jsx
@@ -247,18 +247,18 @@ export default function City() {
                         </div>
                         <div className="w-full rounded-[20px] border-1 border-[#C8E5E5] mt-10">
                             <div className="grid grid-cols-3 w-full bg-[#58AFAE] rounded-t-[20px] h-10 items-center">
-                                <span className="flex ml-5 text-white text-lg font-semibold">Cidade</span>
-                                <span className="flex justify-center items-center text-white text-lg font-semibold">Estado</span>
-                                <span className="flex justify-center text-white text-lg font-semibold">Ações</span>
+                                <div className="flex ml-5 text-white text-lg font-semibold">Cidade</div>
+                                <div className="flex justify-center items-center text-white text-lg font-semibold">Estado</div>
+                                <div className="flex justify-center text-white text-lg font-semibold">Ações</div>
                             </div>
                             <ul className="w-full">
                                 {list.currentList.map((city) => {
                                     const estado = listState.list.find((state) => state.id === city.idEstado);
                                     return (
                                         <li className="grid grid-cols-3 w-full" key={city.id}>
-                                            <span className="flex pl-5 border-r-[1px] border-t-[1px] border-[#C8E5E5] pt-[7.5px] pb-[7.5px] text-gray-700">{city.nomeCidade}</span>
-                                            <span className="flex justify-center items-center border-t-[1px] border-r-[1px] border-[#C8E5E5] text-gray-700">{estado ? estado.nomeEstado : "Estado não encontrado!"}</span>
-                                            <span className="flex items-center justify-center border-t-[1px] gap-2 text-gray-700 border-[#C8E5E5]">
+                                            <div className="flex pl-5 border-r-[1px] border-t-[1px] border-[#C8E5E5] pt-[7.5px] pb-[7.5px] text-gray-700">{city.nomeCidade}</div>
+                                            <div className="flex justify-center items-center border-t-[1px] border-r-[1px] border-[#C8E5E5] text-gray-700">{estado ? estado.nomeEstado : "Estado não encontrado!"}</div>
+                                            <div className="flex items-center justify-center border-t-[1px] gap-2 text-gray-700 border-[#C8E5E5]">
                                                 <button
                                                     className=""
                                                     onClick={() => SelectCity(city, "Editar")}
@@ -271,7 +271,7 @@ export default function City() {
                                                 >
                                                     <TrashSimple size={20} className="hover:text-red-600" />
                                                 </button>
-                                            </span>
+                                            </div>
                                         </li>
                                     );
                                 })}

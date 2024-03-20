d9079aa08277d71b7fd770a9569d05e37cf67ca6
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-03-20 11:28:55 -0300

    :iphone: feat: SidebarAdm tentando ser responsivo

--- Desenvolvimento/FrontEnd/sged/src/pages/routeA/components/Adm/SideBarAdm.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/routeA/components/Adm/SideBarAdm.jsx
@@ -10,6 +10,7 @@ import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import EngineeringIcon from '@mui/icons-material/Engineering';
 import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
+import PersonalVideoIcon from '@mui/icons-material/PersonalVideo';
 
 export default function SideBarAdm() {
     const [isAtendenteOpen, setIsAtendenteOpen] = useState(false);
@@ -24,61 +25,77 @@ export default function SideBarAdm() {
     }
 
     return (
-        <div className="w-[100px] sm:w-[200px] md:w-[250px] lg:w-[260px] fixed h-full bg-[#f6f6f6] text-gray-600">
+        <div className="w-[40px] sm:w-[200px] md:w-[250px] lg:w-[260px] fixed h-full bg-[#f6f6f6] text-gray-600">
             <div className="">
                 <div className="inline-flex items-center justify-center h-[50px] sm:h-[65px] bg-[#2D636B] w-full">
-                    <img src={LogoJales} alt="" className="w-[65px] sm:w-[90px]" />
+                    <img src={LogoJales} alt="" className="w-[45px] sm:w-[90px]" />
                 </div>
-                <div className="flex items-center pl-2 h-[50px] gap-2 bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base cursor-pointer">
-                    <Desktop size={26} weight="fill" />
-                    Página Inicial
+                <div className="flex items-center pl-2 h-[50px] gap-2 w-full bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base cursor-pointer">
+                    <PersonalVideoIcon />
+                    <span className="hidden sm:inline-block">
+                        Página Inicial
+                    </span>
                 </div>
                 <div className="flex items-center w-full pl-2 h-[50px] gap-2 bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base  cursor-pointer"
                     onClick={dropDownAtendente}>
                     <Person3Icon />
-                    Atendente
+                    <span className="hidden sm:inline-block">
+                        Atendente
+                    </span>
                     <div className="flex justify-end w-full pr-2">
                         <KeyboardArrowRightIcon className={isAtendenteOpen ? 'transform rotate-90' : ''} />
                     </div>
                 </div>
-                <div className={`items-center w-full bg-[#f6f6f6] text-base font-base text-gray-500 ${isAtendenteOpen ? '' : 'hidden'}`}
-                    id="submenuAtendente">
-                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                <div className={`items-center w-full bg-[#f6f6f6] text-base font-base text-gray-500 ${isAtendenteOpen ? '' : 'hidden'}`}>
+                    <h1 className="flex items-center pl-2 sm:pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
                         <SaveIcon />
-                        Cadastros
+                        <span className="hidden sm:inline-block">
+                            Cadastros
+                        </span>
                     </h1>
-                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                    <h1 className="flex items-center pl-2 sm:pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
                         <DescriptionIcon />
-                        Documentos
+                        <span className="hidden sm:inline-block">
+                            Documentos
+                        </span>
                     </h1>
                 </div>
                 <div className="flex items-center w-full pl-2 h-[50px] gap-2 bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base  cursor-pointer"
                     onClick={dropDownPerfilPublico}>
                     <div className="inline-flex items-center gap-2 w-full">
                         <SwitchAccountIcon />
-                        Perfil Público
+                        <span className="hidden sm:inline-block">
+                            Perfil Público
+                        </span>
                     </div>
                     <div className="flex justify-end pr-2">
                         <KeyboardArrowRightIcon className={isPerfilPublicoOpen ? 'transform rotate-90' : ''} />
                     </div>
                 </div>
-                <div className={`items-center w-full bg-[#f6f6f6] text-base font-base text-gray-500 ${isPerfilPublicoOpen ? '' : 'hidden'}`}
-                    id="submenuAtendente">
-                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                <div className={`items-center w-full bg-[#f6f6f6] text-base font-base text-gray-500 ${isPerfilPublicoOpen ? '' : 'hidden'}`}>
+                    <h1 className="flex items-center pl-2 sm:pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
                         <GroupsIcon />
-                        Munícipe
+                        <span className="hidden sm:inline-block">
+                            Munícipe
+                        </span>
                     </h1>
-                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                    <h1 className="flex items-center pl-2 sm:pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
                         <SupervisedUserCircleIcon />
-                        Fiscal
+                        <span className="hidden sm:inline-block">
+                            Fiscal
+                        </span>
                     </h1>
-                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                    <h1 className="flex items-center pl-2 sm:pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
                         <EngineeringIcon />
-                        Engenheiro
+                        <span className="hidden sm:inline-block">
+                            Engenheiro
+                        </span>
                     </h1>
-                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                    <h1 className="flex items-center pl-2 sm:pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
                         <AssignmentIndIcon />
-                        Estagiário
+                        <span className="hidden sm:inline-block">
+                            Estagiário
+                        </span>
                     </h1>
                 </div>
             </div>

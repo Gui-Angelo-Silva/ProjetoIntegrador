63a0c841da4cbfe6a482755ad94f1198a2cc3b5f
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-03-20 10:38:29 -0300

    :lipstick: feat: SidebarAdm estilizado com dropdown

--- Desenvolvimento/FrontEnd/sged/src/pages/routeA/components/Adm/SideBarAdm.jsx
+++ Desenvolvimento/FrontEnd/sged/src/pages/routeA/components/Adm/SideBarAdm.jsx
@@ -1,46 +1,85 @@
 import { useState } from "react";
 import LogoJales from "../../../../assets/pages/LogoJales.png"
-import IconHomePage from "../../../../assets/sidebar/IconHomePage";
-import IconSecretary from "../../../../assets/sidebar/IconSecretary";
-import IconPerson from "../../../../assets/sidebar/IconPerson";
-import { CaretDown } from "@phosphor-icons/react";
+import { Desktop } from "@phosphor-icons/react";
+import Person3Icon from '@mui/icons-material/Person3';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import DescriptionIcon from '@mui/icons-material/Description';
+import SaveIcon from '@mui/icons-material/Save';
+import GroupsIcon from '@mui/icons-material/Groups';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
 
 export default function SideBarAdm() {
-    const [isHovered, setIsHovered] = useState(false);
-    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+    const [isAtendenteOpen, setIsAtendenteOpen] = useState(false);
+    const [isPerfilPublicoOpen, setIsPerfilPublicoOpen] = useState(false);
 
-    const toggleSubMenu = () => {
-        setIsSubMenuOpen(!isSubMenuOpen);
-    };
+    function dropDownAtendente() {
+        setIsAtendenteOpen(!isAtendenteOpen);
+    }
+
+    function dropDownPerfilPublico() {
+        setIsPerfilPublicoOpen(!isPerfilPublicoOpen);
+    }
 
     return (
-        <div className="w-[100px] sm:w-[200px] md:w-[250px] lg:w-[260px] fixed h-full bg-[#ddd] text-[#787878]">
+        <div className="w-[100px] sm:w-[200px] md:w-[250px] lg:w-[260px] fixed h-full bg-[#f6f6f6] text-gray-600">
             <div className="">
                 <div className="inline-flex items-center justify-center h-[50px] sm:h-[65px] bg-[#2D636B] w-full">
                     <img src={LogoJales} alt="" className="w-[65px] sm:w-[90px]" />
                 </div>
-                <div className={`inline-flex w-full items-center gap-2 bg-[#f6f6f6] p-2 border-b-2 border-b-[#e3e3e3] cursor-pointer ${isHovered ? 'hover:bg-[#58afae] hover:text-white' : ''}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-                    <IconHomePage estilization="fill-current w-6 h-6" /> Página Inicial
+                <div className="flex items-center pl-2 h-[50px] gap-2 bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base cursor-pointer">
+                    <Desktop size={26} weight="fill" />
+                    Página Inicial
                 </div>
-                <div className="relative">
-                    <div className={`inline-flex w-full h-full items-center gap-2 bg-[#f6f6f6] p-2 border-b-2 border-b-[#e3e3e3] cursor-pointer ${isHovered ? 'hover:bg-[#58afae] hover:text-white' : ''}`} onClick={toggleSubMenu} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-                        <IconSecretary estilization="fill-current w-6 h-6" />
-                        Atendente
-                        <div className={`absolute top-full left-0 bg-white border border-gray-300 p-2 ${isSubMenuOpen ? 'block' : 'hidden'}`}>
-                            <div className="cursor-pointer">Opção 1</div>
-                            <div className="cursor-pointer">Opção 2</div>
-                        </div>
-                        <div className="absolute right-0 flex justify-end items-center h-full mr-2">
-                            <CaretDown size={20} weight="regular" />
-                        </div>
+                <div className="flex items-center w-full pl-2 h-[50px] gap-2 bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base  cursor-pointer"
+                    onClick={dropDownAtendente}>
+                    <Person3Icon />
+                    Atendente
+                    <div className="flex justify-end w-full pr-2">
+                        <KeyboardArrowRightIcon className={isAtendenteOpen ? 'transform rotate-90' : ''} />
                     </div>
                 </div>
-                <div className={`inline-flex w-full h-full items-center gap-2 bg-[#f6f6f6] p-2 border-b-2 border-b-[#e3e3e3] cursor-pointer ${isHovered ? 'hover:bg-[#58afae] hover:text-white' : ''}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-                    <IconPerson estilization="fill-current w-6 h-6" />
-                    Perfil Público
-                    <div className="absolute right-0 flex justify-end items-center h-full mr-2">
-                        <CaretDown size={20} weight="regular" />
+                <div className={`items-center w-full bg-[#f6f6f6] text-base font-base text-gray-500 ${isAtendenteOpen ? '' : 'hidden'}`}
+                    id="submenuAtendente">
+                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                        <SaveIcon />
+                        Cadastros
+                    </h1>
+                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                        <DescriptionIcon />
+                        Documentos
+                    </h1>
+                </div>
+                <div className="flex items-center w-full pl-2 h-[50px] gap-2 bg-[#f6f6f6] hover:bg-[#58AFAE] text-base hover:text-white hover:font-medium font-base  cursor-pointer"
+                    onClick={dropDownPerfilPublico}>
+                    <div className="inline-flex items-center gap-2 w-full">
+                        <SwitchAccountIcon />
+                        Perfil Público
                     </div>
+                    <div className="flex justify-end pr-2">
+                        <KeyboardArrowRightIcon className={isPerfilPublicoOpen ? 'transform rotate-90' : ''} />
+                    </div>
+                </div>
+                <div className={`items-center w-full bg-[#f6f6f6] text-base font-base text-gray-500 ${isPerfilPublicoOpen ? '' : 'hidden'}`}
+                    id="submenuAtendente">
+                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                        <GroupsIcon />
+                        Munícipe
+                    </h1>
+                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                        <SupervisedUserCircleIcon />
+                        Fiscal
+                    </h1>
+                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                        <EngineeringIcon />
+                        Engenheiro
+                    </h1>
+                    <h1 className="flex items-center pl-10 h-[50px] gap-2 cursor-pointer hover:bg-gray-200">
+                        <AssignmentIndIcon />
+                        Estagiário
+                    </h1>
                 </div>
             </div>
         </div>

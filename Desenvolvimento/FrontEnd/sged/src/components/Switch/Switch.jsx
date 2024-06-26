66d4d70aec71fb2e33264f8b0a6e9358f10111d3
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-06-26 09:40:32 -0300

    :sparkles: feat: Componente SWitch adicionado. :lipstick: style: Tela de configurações registrada e estilizada.

---
+++ Desenvolvimento/FrontEnd/sged/src/components/Switch/Switch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Switch = ({ checked, onChange }) => {
+  return (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input type="checkbox" checked={checked} onChange={onChange} className="sr-only peer" />
+      <div className="w-11 h-6 bg-gray-200 rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#96CBCB]"></div>
+    </label>
+  )
+}
+
+export default Switch

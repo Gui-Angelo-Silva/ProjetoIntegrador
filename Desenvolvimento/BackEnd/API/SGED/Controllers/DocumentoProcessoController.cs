ff4a72cb1f53b1dc15efe1791222057bd78ec111
joaoneto132 <joaomasp12@gmail.com>
2024-08-22 16:31:01 -0300

    :sparkless: Model de DocumentoProcesso criada!

---
+++ Desenvolvimento/BackEnd/API/SGED/Controllers/DocumentoProcessoController.cs
@@ -0,0 +1,6 @@
+﻿namespace SGED.Controllers
+{
+	public class DocumentoProcessoController
+	{
+	}
+}
---
+++ Desenvolvimento/BackEnd/API/SGED/Objects/Models/Entities/DocumentoProcesso.cs
@@ -0,0 +1,24 @@
+﻿using SGED.Objects.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGED.Objects.Models.Entities
+{
+	[Table("documentoprocesso")]
+	public class DocumentoProcesso
+	{
+		[Column("iddocumentoprocesso")]
+		public int Id { get; set; }
+
+		[Column("descricaodocumentoprocesso")]
+		public string Descricao { get; set; }
+
+		[Column("observacaodocumentoprocesso")]
+		public string Observacao { get; set; }
+
+		[Column("documentodocumentoprocesso")]
+		public string Documento { get; set; }
+
+		[Column("situacaodocumentoprocesso")]
+		public string Situacao { get; set; }
+	}
+}
